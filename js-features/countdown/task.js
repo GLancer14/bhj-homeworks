3eb86005e7a56af0ee4c0f7242973d5901aa445f
unknown <181259602+GLancer14@users.noreply.github.com>
2025-04-06 22:24:51 +0400

    bhj-1.1.1-hard. First draft

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,10 +1,18 @@
 const currentTimerElement = document.getElementById("timer");
-let currentTimerValue = Number(currentTimerElement.textContent);
-
-let intervalId = setInterval(() => {
-  currentTimerElement.textContent = --currentTimerValue;
-  if (currentTimerValue === 0) {
+const timeOptions = {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+};
+let currentTimerValue = new Date(2025, 0, 1, 0, 0, 15);
+setTime();
+let intervalId = setInterval(setTime, 1000);
+function setTime() {
+  currentTimerValue = new Date(currentTimerValue.setSeconds(currentTimerValue.getSeconds() - 1));
+  currentTimerElement.textContent = Intl.DateTimeFormat("ru", timeOptions).format(currentTimerValue);
+  if (currentTimerValue.getSeconds() === 0 && currentTimerValue.getMinutes() === 0 && currentTimerValue.getHours() === 0) {
     clearInterval(intervalId);
     alert("Вы победили в конкурсе!");
+    document.getElementById("file-download").click();
   }
-}, 1000);
+}
