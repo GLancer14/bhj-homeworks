c48d573464c7b0e98e96c507b63dc3e73088ec74
GLancer14 <181259602+GLancer14@users.noreply.github.com>
2025-04-06 18:39:36 +0400

    bhj-1.1.3. Base task completed

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,25 @@
+const deadElement = document.getElementById("dead");
+const lostElement = document.getElementById("lost");
+const getHole = index => document.getElementById(`hole${index}`)
+for (let i = 1; i < 10; i++) {
+  const holeElement = getHole(i);
+  holeElement.onclick = function() {
+    if (holeElement.className.includes("hole_has-mole")) {
+      deadElement.textContent = Number(deadElement.textContent) + 1;
+    } else {
+      lostElement.textContent = Number(lostElement.textContent) + 1;
+    }
+
+    if (Number(deadElement.textContent) === 10) {
+      runEndgameActions("Вы победили!");
+    } else if (Number(lostElement.textContent) === 5) {
+      runEndgameActions("Вы проиграли! Попробуйте снова.");
+    }
+  };
+}
+
+function runEndgameActions(message) {
+  alert(message);
+  deadElement.textContent = 0;
+  lostElement.textContent = 0;
+}
