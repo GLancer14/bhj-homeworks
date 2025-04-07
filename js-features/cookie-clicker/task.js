49e3bf83d9e6e39be2d6023b2550e1075d7889f2
unknown <181259602+GLancer14@users.noreply.github.com>
2025-04-07 11:03:53 +0400

    bhj-1.1.2-hard. Completed

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,9 +1,16 @@
 const cookieElement = document.getElementById("cookie");
 const cookieInitialWidth = cookieElement.width;
 const clickerCounterElement = document.getElementById("clicker__counter");
+const clickerAverageElement = document.getElementById("clicker__avg");
 let clickerCounterCurrentValue = Number(clickerCounterElement.textContent);
-cookieElement.onclick = function() {
-  cookieElement.width = cookieElement.width != cookieInitialWidth ? cookieInitialWidth : 230;
-  clickerCounterCurrentValue++;
-  clickerCounterElement.textContent = clickerCounterCurrentValue;
-}
+cookieElement.onclick = (function() {
+  let previousClickTime = Date.now();
+  return function() {
+    cookieElement.width = cookieElement.width != cookieInitialWidth ? cookieInitialWidth : 230;
+    clickerCounterCurrentValue++;
+    clickerCounterElement.textContent = clickerCounterCurrentValue;
+    const currentClickTime = Date.now();
+    clickerAverageElement.textContent = (1 / ((currentClickTime - previousClickTime) / 1000)).toFixed(2);
+    previousClickTime = currentClickTime;
+  }
+})();
