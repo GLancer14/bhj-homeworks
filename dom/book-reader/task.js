39abee8e6d8c64709b30731882fb003c207cc706
unknown <181259602+GLancer14@users.noreply.github.com>
2025-04-18 20:34:59 +0400

    bhj-2.1.3-hard. Task solved

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,14 +1,62 @@
 const bookElement = document.getElementById("book");
 const bookFontControlElement = document.querySelector(".book__control_font-size");
-bookFontControlElement.addEventListener("click", function(e) {
+const bookColorControlElement = document.querySelector(".book__control_color");
+const bookBgColorControlElement = document.querySelector(".book__control_background");
+bookFontControlElement.addEventListener("click", function (e) {
   e.preventDefault();
-  if (!e.target.classList.contains("font-size_active")) {
+  if (!e.target.classList.contains("font-size_active") && e.target.tagName === "A") {
     e.target.classList.add("font-size_active");
-    bookElement.className = e.target.dataset.size ? `book book_fs-${e.target.dataset.size}` : `book`;
     Array.from(e.currentTarget.children).forEach(item => {
       if (item !== e.target) {
         item.classList.remove("font-size_active");
       }
     });
+
+    bookElement.classList.forEach(item => {
+      if (item.includes("fs")) {
+        bookElement.classList.remove(item);
+      }
+    });
+    if (e.target.dataset.size) {
+      bookElement.classList.add(`book_fs-${e.target.dataset.size}`);
+    }
+  }
+});
+
+bookColorControlElement.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("color_active") && e.target.tagName === "A") {
+    e.target.classList.add("color_active");
+    Array.from(e.currentTarget.children).forEach(item => {
+      if (item !== e.target) {
+        item.classList.remove("color_active");
+      }
+    });
+
+    bookElement.classList.forEach(item => {
+      if (item.includes("color")) {
+        bookElement.classList.remove(item);
+      }
+    });
+    bookElement.classList.add(`book_color-${e.target.dataset.textColor}`);
+  }
+});
+
+bookBgColorControlElement.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("color_active") && e.target.tagName === "A") {
+    e.target.classList.add("color_active");
+    Array.from(e.currentTarget.children).forEach(item => {
+      if (item !== e.target) {
+        item.classList.remove("color_active");
+      }
+    });
+
+    bookElement.classList.forEach(item => {
+      if (item.includes("bg")) {
+        bookElement.classList.remove(item);
+      }
+    });
+    bookElement.classList.add(`book_bg-${e.target.dataset.bgColor}`);
   }
 });
