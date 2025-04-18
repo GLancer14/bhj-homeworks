dd51e5ab03f1aad4f293f0ba030394adaad9a155
unknown <181259602+GLancer14@users.noreply.github.com>
2025-04-18 19:24:05 +0400

    bhj-2.1.2-hard. Task solved

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,14 +1,19 @@
-const rotatorCasesElements = document.querySelectorAll(".rotator__case");
-const setIterator = function(elementsCollection) {
+const rotatorElements = document.querySelectorAll(".rotator");
+function setIterator(elementsCollection) {
+  const lastCaseIndex = elementsCollection.length - 1;
   let currentCaseIndex = 0;
-  let previousCaseIndex = elementsCollection.length - 1;
-  return function() {
-    setInterval(() => {
-      currentCaseIndex = (currentCaseIndex === elementsCollection.length - 1) ? 0 : currentCaseIndex + 1;
-      previousCaseIndex = (previousCaseIndex === elementsCollection.length - 1) ? 0 : previousCaseIndex + 1;
-      elementsCollection[currentCaseIndex].classList.add("rotator__case_active");
-      elementsCollection[previousCaseIndex].classList.remove("rotator__case_active");
-    }, 1000);
+  let previousCaseIndex = lastCaseIndex;
+  function setChangingTimeout() {
+    const currentCase = elementsCollection[currentCaseIndex];
+    currentCase.classList.add("rotator__case_active");
+    currentCase.style.color = currentCase.dataset.color;
+    elementsCollection[previousCaseIndex].classList.remove("rotator__case_active");
+    setTimeout(setChangingTimeout, currentCase.dataset.speed);
+    currentCaseIndex = (currentCaseIndex === lastCaseIndex) ? 0 : currentCaseIndex + 1;
+    previousCaseIndex = (previousCaseIndex === lastCaseIndex) ? 0 : previousCaseIndex + 1;
   }
-};
-setIterator(rotatorCasesElements)();
+
+  setChangingTimeout();
+}
+
+rotatorElements.forEach(item => setIterator(item.children));
