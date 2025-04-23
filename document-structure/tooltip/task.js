2666f978df208eef47515e4d827d07e83e8193e9
unknown <181259602+GLancer14@users.noreply.github.com>
2025-04-23 17:09:53 +0400

    bhj-2.3.1-hard. Task solved (my version)

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,35 @@
+const anchors = document.querySelectorAll(".has-tooltip");
+const positions = ["top", "right", "bottom", "left"];
+anchors.forEach((item, index) => {
+  const tooltip = document.createElement("div");
+  tooltip.classList.add("tooltip");
+  tooltip.textContent = item.getAttribute("title");
+  tooltip.setAttribute("data-position", positions[index % positions.length])
+  item.insertAdjacentElement("afterbegin", tooltip);
+  item.addEventListener("click", function (e) {
+    e.preventDefault();
+    tooltip.classList.toggle("tooltip_active");
+    switch (tooltip.dataset.position) {
+      case "top":
+        [tooltip.style.left, tooltip.style.top] = [0, `${0 - tooltip.offsetHeight}px`];
+        break;
+      case "left":
+        [tooltip.style.left, tooltip.style.top] = [`${0 - tooltip.offsetWidth}px`, 0];
+        break;
+      case "right":
+        [tooltip.style.left, tooltip.style.top] = [`${item.offsetWidth}px`, 0];
+        break;
+      case "bottom":
+        [tooltip.style.left, tooltip.style.top] = [0, `${item.offsetHeight}px`];
+        break;
+      default:
+        [tooltip.style.left, tooltip.style.top] = [0, `${item.offsetHeight}px`];
+    }
+
+    document.querySelectorAll(".tooltip").forEach(tooltipElement => {
+      if (tooltipElement !== tooltip) {
+        tooltipElement.classList.remove("tooltip_active");
+      }
+    });
+  });
+});
