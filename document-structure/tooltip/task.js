8c012bf58ee3fa726a0ccc332c88561885644ecf
unknown <181259602+GLancer14@users.noreply.github.com>
2025-04-24 19:40:51 +0400

    bhj-2.3. Correction #1 done

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -5,25 +5,25 @@ anchors.forEach((item, index) => {
   tooltip.classList.add("tooltip");
   tooltip.textContent = item.getAttribute("title");
   tooltip.setAttribute("data-position", positions[index % positions.length])
-  item.insertAdjacentElement("afterbegin", tooltip);
+  item.insertAdjacentElement("afterend", tooltip);
   item.addEventListener("click", function (e) {
     e.preventDefault();
     tooltip.classList.toggle("tooltip_active");
     switch (tooltip.dataset.position) {
       case "top":
-        [tooltip.style.left, tooltip.style.bottom] = [0, `${item.offsetHeight}px`];
+        [tooltip.style.left, tooltip.style.top] = [`${item.offsetLeft}px`, `${item.offsetTop - tooltip.offsetHeight}px`];
         break;
       case "left":
-        [tooltip.style.right, tooltip.style.top] = [`${item.offsetWidth}px`, 0];
+        [tooltip.style.left, tooltip.style.top] = [`${item.offsetLeft - tooltip.offsetWidth}px`, `${item.offsetTop}px`];
         break;
       case "right":
-        [tooltip.style.left, tooltip.style.top] = [`${item.offsetWidth}px`, 0];
+        [tooltip.style.left, tooltip.style.top] = [`${item.offsetLeft + item.offsetWidth}px`, `${item.offsetTop}px`];
         break;
       case "bottom":
-        [tooltip.style.left, tooltip.style.top] = [0, `${item.offsetHeight}px`];
+        [tooltip.style.left, tooltip.style.top] = [`${item.offsetLeft}px`, `${item.offsetTop + item.offsetHeight}px`];
         break;
       default:
-        [tooltip.style.left, tooltip.style.top] = [0, `${item.offsetHeight}px`];
+        [tooltip.style.left, tooltip.style.top] = [`${item.offsetLeft}px`, `${item.offsetTop + item.offsetHeight}px`];
     }
 
     document.querySelectorAll(".tooltip").forEach(tooltipElement => {
