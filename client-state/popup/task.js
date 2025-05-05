d4dfeaedba9070e67663a096ca5de3e1254bede9
unknown <181259602+GLancer14@users.noreply.github.com>
2025-05-05 12:47:22 +0400

    bhj-3.2.2. Task solved

---
+++ client-state/popup/task.js
@@ -0,0 +1,9 @@
+const modalCloseButton = document.querySelector(".modal__close");
+const subscribeModal = document.getElementById("subscribe-modal");
+modalCloseButton.addEventListener("click", function () {
+  document.cookie = "modal=closed";
+  subscribeModal.classList.remove("modal_active");
+});
+document.addEventListener("DOMContentLoaded", function () {
+  document.cookie.includes("modal=closed") ? subscribeModal.classList.remove("modal_active") : subscribeModal.classList.add("modal_active")
+});
